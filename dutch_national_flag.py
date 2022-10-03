95bb30a03897004c3e6443eba0b5bce7c10fa6b7
Rohit <trohit@users.noreply.github.com>
2022-10-03 10:28:37 +0530

    Update dutch_national_flag.py

--- dutch_national_flag.py
+++ dutch_national_flag.py
@@ -48,4 +48,33 @@ def dutch_flag_sort(balls):
             k-=1
         else: # a[j] == mid aka G
             j+=1
+    # T:O(n)
     return a
+
+# Alternate faster implemention
+
+def dutch_flag_sort(balls):
+    """
+    Args:
+     balls(list_char)
+    Returns:
+     list_char
+    """
+    # Write your code here.
+    n= len(balls)
+    bptr =n-1
+    # loop for bptr
+    for i in range(n-1,-1,-1):
+         #print(f"i {i} balls{balls[i]}")
+         if balls[i] == 'B':
+             balls[bptr], balls[i] = balls[i], balls[bptr]
+             bptr -= 1
+    
+    rptr = 0
+    for i in range(0, bptr+1):
+        if balls[i] == 'R':
+            balls[rptr], balls[i] = balls[i], balls[rptr]
+            rptr += 1
+    return balls
+          
+    
