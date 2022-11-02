423b9e4fa24d03207af65bab89c2691d38469689
Rohit <trohit@users.noreply.github.com>
2022-11-02 20:05:48 +0530

    Create postorder_traversal_iter.py

---
+++ trees/postorder_traversal_iter.py
@@ -0,0 +1,52 @@
+"""
+PostOrder Traversal Without Recursion
+Given a binary tree, find its post-order traversal without using recursion.
+
+Input:
+{
+"root": [100,
+200, 300,
+400, 500]
+}
+
+Output:
+[400, 500, 200, 300, 100]
+"""
+
+"""
+For your reference:
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+"""
+def disp(st):
+    print("[", end="")
+    for i in st:
+        print(f"{i.value}", end=",")
+    print("]", end="")
+        
+def postorder_traversal(root):
+    """
+    Args:
+     root(BinaryTreeNode_int32)
+    Returns:
+     list_int32
+    """
+    # Write your code here.
+    res, st = [], [root]
+    while st:
+        node = st[-1] # peek into stack
+        #print(f"n:{node.value}", end="=>"); disp(st); print(f"res:{res}")
+        if node.left:
+            st.append(node.left)
+            node.left = None
+        elif node.right:
+            st.append(node.right)
+            node.right = None
+        else:
+            st.pop()
+            res.append(node.value)
+    return res
+
